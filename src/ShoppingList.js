6856b85d500d0db6f369f43cb7070b781d617cac
fabianbohn <Mystek.fb@googlemail.com>
2022-05-19 11:09:46 +0200

    react lektion 1

--- src/ShoppingList.js
+++ src/ShoppingList.js
@@ -11,8 +11,103 @@ class ShoppingList extends React.Component {
       //füge hier deinen HTML-Code ein
       <div id="container">
         {/* ToDo: füge hier drunter Deinen HTML-Code ein */}
-        Füge hier Deinen HTML-Code ein<br/>
-        <GruppenTag/>
+        <head>
+          <meta charSet="UTF-8"/>
+          <title>Title</title>
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/icon?family=Material+Icons"
+          />
+        </head>
+
+        <body>
+        <header>
+          <h1>Lager</h1>
+          <nav>
+            <input type="text" placeholder="Artikel Hinzufügen" />
+            <button className="material-icons" style="vertical-align: bottom">
+              add_circle
+            </button>
+          </nav>
+          <hr />
+        </header>
+
+        <main>
+          <section>
+            <h2>
+              Einkaufen<i style="float: right" className="material-icons">
+              expand_less
+            </i>
+            </h2>
+
+            <dl>
+              <dt>
+                <p style="background-color: lightblue">
+                  Obst & Gemüse
+                  <i style="float: right" className="material-icons">expand_less</i>
+                </p>
+              </dt>
+              <dd>
+                <label><input type="checkbox" />Broccoli</label>
+              </dd>
+
+              <dt>
+                <p style="background-color: lightblue">
+                  Getreideprodukte
+                  <i style="float: right" className="material-icons">expand_less</i>
+                </p>
+              </dt>
+
+              <dd>
+                <label><input type="checkbox" />Reis</label>
+              </dd>
+
+              <dt>
+                <p style="background-color: lightblue">
+                  Milchprodukte
+                  <i style="float: right" className="material-icons">expand_less</i>
+                </p>
+              </dt>
+
+              <dd>
+                <label><input type="checkbox" />Streukäse</label>
+              </dd>
+            </dl>
+            <hr />
+          </section>
+
+          <section>
+            <h2>
+              Erledigt<i style="float: right" className="material-icons">expand_less</i>
+            </h2>
+
+            <dl>
+              <dt>
+                <p style="background-color: lightblue">
+                  Hülsenfrüchte<i style="float: right" className="material-icons"
+                >expand_less</i
+                >
+                </p>
+              </dt>
+
+              <dd>
+                <label><input type="checkbox" /><s>Tofu</s></label>
+              </dd>
+            </dl>
+            <hr />
+          </section>
+        </main>
+
+        <footer>
+          <nav>
+            <button><span className="material-icons">bookmark_add</span>Gruppe</button>
+            <button><span className="material-icons">sort</span>Sortieren</button>
+            <button><span className="material-icons">settings</span>Einstellungen</button>
+          </nav>
+        </footer>
+      </body>
+
+    <GruppenTag/>
       </div>
     )
   }
